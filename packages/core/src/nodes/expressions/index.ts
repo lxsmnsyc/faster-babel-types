1fa311af8cbc52b5f9d9690274f4df0707692ae9
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-01 16:01:06 +0800

    Add `StringLiteral`

--- packages/core/src/nodes/expressions/index.ts
+++ packages/core/src/nodes/expressions/index.ts
@@ -5,3 +5,4 @@ export * from './call-expression';
 export * from './conditional-expression';
 export * from './function-expression';
 export * from './identifier';
+export * from './string-literal';
