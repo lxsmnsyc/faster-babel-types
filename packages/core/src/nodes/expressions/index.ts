008bbb225f8f3fd751dd7507f31785d389ee4ba7
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-01 18:32:34 +0800

    Add `ImportExpression`

--- packages/core/src/nodes/expressions/index.ts
+++ packages/core/src/nodes/expressions/index.ts
@@ -8,6 +8,7 @@ export * from './class-expression';
 export * from './conditional-expression';
 export * from './function-expression';
 export * from './identifier';
+export * from './import-expression';
 export * from './logical-expression';
 export * from './member-expression';
 export * from './new-expression';
