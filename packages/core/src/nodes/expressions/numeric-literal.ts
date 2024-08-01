53f5339df00c0e9891b96104b06874411248e384
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-01 16:03:42 +0800

    Update numeric-literal.ts

--- packages/core/src/nodes/expressions/numeric-literal.ts
+++ packages/core/src/nodes/expressions/numeric-literal.ts
@@ -2,23 +2,23 @@ import type * as t from '@babel/types';
 import { assert } from '../../assert-node-type';
 import { is } from '../../is-node-type';
 
-const TYPE = 'NumberLiteral';
+const TYPE = 'NumericLiteral';
 
-export function numericLiteral(value: number): t.NumberLiteral {
+export function numericLiteral(value: number): t.NumericLiteral {
   return {
     type: TYPE,
     value,
   };
 }
 
-export function isNumberLiteral(
+export function isNumericLiteral(
   node: t.Node | null | undefined,
-): node is t.NumberLiteral {
+): node is t.NumericLiteral {
   return is(TYPE, node);
 }
 
-export function assertNumberLiteral(
+export function assertNumericLiteral(
   node: t.Node | null | undefined,
-): asserts node is t.NumberLiteral {
+): asserts node is t.NumericLiteral {
   assert(TYPE, node);
 }
