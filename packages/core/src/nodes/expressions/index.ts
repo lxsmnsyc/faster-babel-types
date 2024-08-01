b166fd2a8e6abb18e2b0e8eb63de9573eb75bd67
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-01 15:29:47 +0800

    Add `ConditionalExpression`

--- packages/core/src/nodes/expressions/index.ts
+++ packages/core/src/nodes/expressions/index.ts
@@ -2,3 +2,4 @@ export * from './array-expression';
 export * from './assignment-expression';
 export * from './binary-expression';
 export * from './call-expression';
+export * from './conditional-expression';
