b50eefb5fcd8ad8d1717bb946f60e12586d711e3
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 00:58:21 +0800

    Add `TSTypeAssertion`

--- packages/core/src/nodes/expressions/index.ts
+++ packages/core/src/nodes/expressions/index.ts
@@ -36,6 +36,7 @@ export * from './tagged-template-expression';
 export * from './template-literal';
 export * from './this-expression';
 export * from './ts-non-null-expression';
+export * from './ts-type-assertion';
 export * from './tuple-expression';
 export * from './typecast-expression';
 export * from './unary-expression';
