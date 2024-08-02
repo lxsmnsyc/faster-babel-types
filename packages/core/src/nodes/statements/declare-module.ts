0ab7ff1b6eea83a75df8730133d5332a1dc90499
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 13:35:21 +0800

    Add `DeclareModule`

---
+++ packages/core/src/nodes/statements/declare-module.ts
@@ -0,0 +1,32 @@
+import type * as t from '@babel/types';
+import { assert } from '../../assert-node-type';
+import { is } from '../../is-node-type';
+
+const TYPE = 'DeclareModule';
+
+export type DeclareModuleKind = 'CommonJS' | 'ES';
+
+export function declareModule(
+  id: t.Identifier | t.StringLiteral,
+  body: t.BlockStatement,
+  kind?: DeclareModuleKind | null,
+): t.DeclareModule {
+  return {
+    type: TYPE,
+    id,
+    body,
+    kind,
+  };
+}
+
+export function isDeclareModule(
+  node: t.Node | null | undefined,
+): node is t.DeclareModule {
+  return is(TYPE, node);
+}
+
+export function assertDeclareModule(
+  node: t.Node | null | undefined,
+): asserts node is t.DeclareModule {
+  assert(TYPE, node);
+}
