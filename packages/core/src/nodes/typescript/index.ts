6caff167beb85a8bd3bc126cbe2d24ce08e7fafd
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 18:44:00 +0800

    Add `TSTypeParameter`

--- packages/core/src/nodes/typescript/index.ts
+++ packages/core/src/nodes/typescript/index.ts
@@ -1,4 +1,5 @@
 export * from './ts-declare-method';
 export * from './ts-parameter-property';
 export * from './ts-type-annotation';
+export * from './ts-type-parameter';
 export * from './ts-type-parameter-declaration';
