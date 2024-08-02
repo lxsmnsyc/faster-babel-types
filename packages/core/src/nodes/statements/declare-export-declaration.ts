895f6184ed8beca864ef8c1c2ba46943bd1a82a4
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 13:44:06 +0800

    Add `DeclareExportDeclaration`

---
+++ packages/core/src/nodes/statements/declare-export-declaration.ts
@@ -0,0 +1,30 @@
+import type * as t from '@babel/types';
+import { assert } from '../../assert-node-type';
+import { is } from '../../is-node-type';
+
+const TYPE = 'DeclareExportDeclaration';
+
+export function declareExportDeclaration(
+  declaration?: t.Flow | null,
+  specifiers?: Array<t.ExportSpecifier | t.ExportNamespaceSpecifier> | null,
+  source?: t.StringLiteral | null,
+): t.DeclareExportDeclaration {
+  return {
+    type: TYPE,
+    declaration,
+    specifiers,
+    source,
+  };
+}
+
+export function isDeclareExportDeclaration(
+  node: t.Node | null | undefined,
+): node is t.DeclareExportDeclaration {
+  return is(TYPE, node);
+}
+
+export function assertDeclareExportDeclaration(
+  node: t.Node | null | undefined,
+): asserts node is t.DeclareExportDeclaration {
+  assert(TYPE, node);
+}
