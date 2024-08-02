eab66533facc445a0b020c86f17eac2868a6b9db
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 14:42:11 +0800

    Add `TypeAnnotation`

---
+++ packages/core/src/nodes/flow/type-annotation.ts
@@ -0,0 +1,24 @@
+import type * as t from '@babel/types';
+import { assert } from '../../assert-node-type';
+import { is } from '../../is-node-type';
+
+const TYPE = 'TypeAnnotation';
+
+export function typeAnnotation(typeAnnotation: t.FlowType): t.TypeAnnotation {
+  return {
+    type: TYPE,
+    typeAnnotation,
+  };
+}
+
+export function isTypeAnnotation(
+  node: t.Node | null | undefined,
+): node is t.TypeAnnotation {
+  return is(TYPE, node);
+}
+
+export function assertTypeAnnotation(
+  node: t.Node | null | undefined,
+): asserts node is t.TypeAnnotation {
+  assert(TYPE, node);
+}
--- packages/core/src/nodes/index.ts
+++ packages/core/src/nodes/index.ts
@@ -1,4 +1,5 @@
 export * from './expressions';
+export * from './flow';
 export * from './javascript';
 export * from './statements';
 export * from './typescript';
