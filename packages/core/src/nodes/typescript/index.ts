3b87eef05263d02872fea491c165983a83861dd5
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 18:45:17 +0800

    Add `TSIndexSignature`

--- packages/core/src/nodes/typescript/index.ts
+++ packages/core/src/nodes/typescript/index.ts
@@ -1,4 +1,5 @@
 export * from './ts-declare-method';
+export * from './ts-index-signature';
 export * from './ts-parameter-property';
 export * from './ts-type-annotation';
 export * from './ts-type-parameter';
