6ed39b21cbfb4b63b38f9891bf817f812eb76d0f
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 14:21:41 +0800

    Add `PrivateName`

--- packages/core/src/nodes/javascript/index.ts
+++ packages/core/src/nodes/javascript/index.ts
@@ -2,5 +2,6 @@ export * from './array-pattern';
 export * from './assignment-pattern';
 export * from './directive';
 export * from './directive-literal';
+export * from './private-name';
 export * from './rest-element';
 export * from './spread-element';
