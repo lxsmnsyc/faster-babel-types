a7bdeb65ea0bf1d557f6f7065c493cc3c7d5e7e1
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 13:33:27 +0800

    Add `DeclareInterface`

---
+++ packages/core/src/nodes/statements/declare-interface.ts
@@ -0,0 +1,32 @@
+import type * as t from '@babel/types';
+import { assert } from '../../assert-node-type';
+import { is } from '../../is-node-type';
+
+const TYPE = 'DeclareInterface';
+
+export function declareInterface(
+  id: t.Identifier,
+  typeParameters: t.TypeParameterDeclaration | null | undefined,
+  interfaceExtends: t.InterfaceExtends[] | null | undefined,
+  body: t.ObjectTypeAnnotation,
+): t.DeclareInterface {
+  return {
+    type: TYPE,
+    id,
+    typeParameters,
+    extends: interfaceExtends,
+    body,
+  };
+}
+
+export function isDeclareInterface(
+  node: t.Node | null | undefined,
+): node is t.DeclareInterface {
+  return is(TYPE, node);
+}
+
+export function assertDeclareInterface(
+  node: t.Node | null | undefined,
+): asserts node is t.DeclareInterface {
+  assert(TYPE, node);
+}
