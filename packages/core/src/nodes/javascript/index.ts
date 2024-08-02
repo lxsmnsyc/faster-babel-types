fa8c7ebee037105973a6effe65bb0f8970f69470
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-02 14:22:49 +0800

    Add `ObjectPattern`

--- packages/core/src/nodes/javascript/index.ts
+++ packages/core/src/nodes/javascript/index.ts
@@ -2,6 +2,7 @@ export * from './array-pattern';
 export * from './assignment-pattern';
 export * from './directive';
 export * from './directive-literal';
+export * from './object-pattern';
 export * from './private-name';
 export * from './rest-element';
 export * from './spread-element';
