e9b38a79f6a657b9b09b560dd37783bbc61edf81
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-08-05 20:46:29 +0800

    Restructure JSX

--- packages/core/src/nodes/expressions/index.ts
+++ packages/core/src/nodes/expressions/index.ts
@@ -15,8 +15,6 @@ export * from './function-expression';
 export * from './identifier';
 export * from './import';
 export * from './import-expression';
-export * from './jsx-element';
-export * from './jsx-fragment';
 export * from './logical-expression';
 export * from './member-expression';
 export * from './meta-property';
